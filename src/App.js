8169f49545daa1142fc1eecc7fbb3206fb87dec4
houbly <h.blyden@gmail.com>
2019-03-11 13:37:04 +0000

    switched to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CssBaseline />
+        <header className="App-header">Fab Factory</header>
       </div>
     );
   }
