f881f47c4b1e02754c42493bd7d3e6d571f7f486
Philip Bruland <philipbruland@gmail.com>
2019-03-13 18:23:19 +0000

    Issue #1: Back End Infrastructure
    - Create simple reporting infrastructure taking data from the json file and placing it into an object.

--- src/decision_tree/tree.js
+++ src/decision_tree/tree.js
@@ -1,32 +1,46 @@
 import paths from './paths/paths.json';
 
+// When this goes server-side, this should no longer be global
+const report = {};
+
 function getCurrentState(state) {
   return paths[state];
 }
 
-function changeState(currentState, nextState) {
+function changeState(currentState, answer) {
+  let nextState = null;
+  let reportData = null;
+  let found = 0;
+
   if (paths[currentState].type !== 'question') {
     throw new Error("Can't select next state, current state is not a question");
   }
 
-  let found = 0;
   const { options } = paths[currentState].data;
   options.forEach((option) => {
-    if (option.nextState === nextState) {
+    if (option.text === answer) {
       found += 1;
+      ({ nextState } = option);
+      ({ reportData } = option);
     }
   });
 
-  if (found === 0) {
+  if (nextState === null) {
     throw new Error('Next state not available in node');
+  } else if (found > 1) {
+    throw new Error('More than one state found with that name');
   }
 
+  report[paths[currentState].name] = reportData;
   return paths[nextState];
 }
 
-// Need getReport function
+function getReport() {
+  return report;
+}
 
 export default {
   getCurrentState,
   changeState,
+  getReport,
 };
--- src/decision_tree/tree.test.js
+++ src/decision_tree/tree.test.js
@@ -4,11 +4,13 @@ import Tree from './tree';
 it('does some backend logic', () => {
   let errors = 0;
   let state = null;
+  let report = null;
+  const expectedReport = { question1: { example: 1 } };
 
   state = Tree.getCurrentState('question1');
   expect(state.name).toEqual('question1');
 
-  state = Tree.changeState(state.name, 'question2');
+  state = Tree.changeState(state.name, 'option2');
   expect(state.name).toEqual('question2');
 
   try {
@@ -17,4 +19,7 @@ it('does some backend logic', () => {
     errors += 1;
   }
   expect(errors).toEqual(1);
+
+  report = Tree.getReport();
+  expect(report).toEqual(expectedReport);
 });
