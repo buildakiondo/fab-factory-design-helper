2f92e44a43b7f368d3775550c63378121690e62d
houbly <h.blyden@gmail.com>
2019-03-13 15:41:09 +0000

    Merged changes

--- src/DefaultTheme.js
+++ src/DefaultTheme.js
@@ -1,27 +1,37 @@
-import { grey } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import { grey } from "@material-ui/core/colors";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#4223fb',
-      contrastText: '#ffffff',
+      main: "#4223fb",
+      contrastText: "#ffffff"
     },
     secondary: {
-      main: '#96d5d2',
-      contrastText: '#333',
+      main: "#96d5d2",
+      contrastText: "#333"
     },
     tertiary: {
-      main: '#4223fb',
-      contrastText: '#333',
+      main: "#4223fb",
+      contrastText: "#333"
     },
-    bodyText: grey[100],
+    bodyText: grey[100]
   },
   typography: {
     useNextVariants: true,
-    fontFamily: '"Montserrat", sans-serif',
+    fontFamily: `'Nunito Sans', sans-serif`,
+    fontWeight: 300
   },
-  overrides: {},
+  overrides: {
+    MuiTypography: {
+      root: {
+        fontWeight: 300
+      },
+      h3: {
+        fontWeight: 300
+      }
+    }
+  }
 });
 
 export default theme;
